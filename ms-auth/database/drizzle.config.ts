bc7bdfca236c260639cfede0c420c3c6ae74189d
MarcosVTavares <marquinhotavares03@gmail.com>
2026-03-25 21:24:32 -0300

    module

--- ms-auth/database/drizzle.config.ts
+++ ms-auth/database/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-	schema: "./src/shared/infra/database/schemas/*.ts",
+	schema: "./schemas/*.ts",
 	out: "./drizzle",
 	dialect: "postgresql",
 	dbCredentials: {
